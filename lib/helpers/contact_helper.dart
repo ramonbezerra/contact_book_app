271af2f9f5b17dbc8ca5f0f68ca662dc2c63ef59
ramonbezerra <57471737+ramonbezerra@users.noreply.github.com>
2020-04-27 16:15:50 -0300

    contacts list from database

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,13 +1,100 @@
 import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
-final String idColumn = "idColumn";
-final String nameColumn = "nameColumn";
-final String emailColumn = "emailColumn";
-final String phoneColumn = "phoneColumn";
-final String imageColumn = "imageColumn";
+final String contactTable = "Contact";
+
+final String idColumn = "id";
+final String nameColumn = "name";
+final String emailColumn = "email";
+final String phoneColumn = "phone";
+final String imageColumn = "image";
 
 class ContactHelper {
+  static final ContactHelper _instance = ContactHelper.internal();
 
+  factory ContactHelper() => _instance;
+
+  ContactHelper.internal();
+
+  Database _db;
+  
+  Future<Database> get db async {
+    if (_db != null)
+      return _db;
+    else
+      return await initDb();
+  }
+
+  Future<Database> initDb() async {
+    final databasesPath = await getDatabasesPath();
+    final path = join(databasesPath, "contacts.db");
+
+    return await openDatabase(path, version: 1, onCreate: (db, newerVersion) async {
+      await db.execute(
+        "CREATE TABLE $contactTable($idColumn INTEGER PRIMARY KEY, $nameColumn TEXT, $emailColumn TEXT, $phoneColumn TEXT, $imageColumn TEXT)"
+      );
+    });
+  }
+
+  Future<Contact> saveContact(Contact contact) async {
+    Database dbContact = await db;
+    contact.id = await dbContact.insert(contactTable, contact.toMap());
+    return contact;
+  }
+
+  Future<Contact> getContact(int id) async {
+    Database dbContact = await db;
+    
+    List<Map> maps = await dbContact.query(
+      contactTable, 
+      columns: [idColumn, nameColumn, emailColumn, phoneColumn, imageColumn],
+      where: "$idColumn = ?", 
+      whereArgs: [id]);
+
+    if (maps.length > 0)
+      return Contact.fromMap(maps.first);
+    else 
+      return null;
+  }
+
+  Future<List> getContacts() async {
+    Database dbContact = await db;
+    
+    List maps = await dbContact.rawQuery("SELECT * FROM $contactTable");
+
+    List<Contact> listContacts = List();
+
+    for (Map map in maps) {
+      listContacts.add(Contact.fromMap(map));
+    }
+
+    return listContacts;
+  }
+
+  Future<int> deleteContact(int id) async {
+    Database dbContact = await db;
+    
+    return await dbContact.delete(contactTable, where: "$idColumn = ?", whereArgs: [id]);
+  }
+
+  Future<int> updateContact(Contact contact) async {
+    Database dbContact = await db;
+
+    return await dbContact.update(contactTable, contact.toMap(), where: "$idColumn = ?", 
+      whereArgs: [contact.id]);
+  }
+
+  Future<int> getNumber() async {
+    Database dbContact = await db;
+
+    return Sqflite.firstIntValue(await dbContact.rawQuery("SELECT COUNT(*) FROM $contactTable"));
+  }  
+
+  Future close() async {
+    Database dbContact = await db;
+
+    dbContact.close();
+  }
 }
 
 class Contact {
@@ -16,6 +103,8 @@ class Contact {
   String email;
   String phone;
   String image;
+
+  Contact(this.name, this.email, this.phone, this.image);
 
   Contact.fromMap(Map map) {
     id = map[idColumn];
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:contact_book_app/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -6,11 +9,87 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ContactHelper helper = ContactHelper();
+
+  List<Contact> contacts = List();
+
+  @override
+  void initState() {
+    super.initState();
+
+    helper.getContacts().then((list) {
+      setState(() {
+        contacts = list;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(),
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+        padding: EdgeInsets.all(10.0),
+        itemCount: contacts.length,
+        itemBuilder: (context, index) {
+          return _contactCard(context, index);
+        }
+      )
+    );
+  }
+
+  Widget _contactCard(context, index) {
+    return GestureDetector(
+      child: Card(
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Row(
+            children: <Widget>[
+              Container(
+                width: 80.0,
+                height: 80.0,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                    image: contacts[index].image != null ? 
+                      FileImage(File(contacts[index].image)) :
+                        Image.asset("images/person.png")
+                  )
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 10.0),
+                child: Column(
+                  children: <Widget>[
+                    Text(contacts[index].name ?? "", 
+                      style: TextStyle(fontSize: 22.0,
+                              fontWeight: FontWeight.bold)
+                    ),
+                    Text(contacts[index].email ?? "", 
+                      style: TextStyle(fontSize: 18.0,
+                              fontWeight: FontWeight.bold)
+                    ),
+                    Text(contacts[index].phone ?? "", 
+                      style: TextStyle(fontSize: 18.0,
+                              fontWeight: FontWeight.bold)
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
